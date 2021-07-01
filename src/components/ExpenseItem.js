dfbe7f8ac78ee03cbef5c818de92d6d6f8c5669e
Yashasree Tummala <yashasreetummala1991@gmail.com>
2021-07-01 18:51:13 +0530

    [Yash] Add. Expanded Date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{year}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.name}</h2>
         <div className="expense-item__price">Rs. {props.price}/-</div>
