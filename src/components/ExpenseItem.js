cc4e54d0cc59058f5295c631d95d11d09a60e751
Yashasree Tummala <yashasreetummala1991@gmail.com>
2021-07-02 13:25:45 +0530

    [Yash] Add. Expanded Date extracted to a new component (A->B->C)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const year = props.date.getFullYear();
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-
   return (
     <div className="expense-item">
       <div>
-        <div>{year}</div>
-        <div>{month}</div>
-        <div>{day}</div>
+        <ExpenseDate date={props.date} />
       </div>
       <div className="expense-item__description">
         <h2>{props.name}</h2>
